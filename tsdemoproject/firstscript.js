2200114257edfb6e7360a0fd0ec2ccd81c6d5673
Al-Fareed <alfareedss472@gmail.com>
2023-04-28 20:31:26 +0530

    first program in ts

---
+++ tsdemoproject/firstscript.js
@@ -0,0 +1,5 @@
+function addNumbers(a, c) {
+    return a + c;
+}
+var sums = addNumber(10, 20);
+console.log('Sum of two numbers:' + sum);
---
+++ tsdemoproject/firstscript.ts
@@ -0,0 +1,6 @@
+function addNumber(a:number, b:number)
+{
+    return a+b;
+}
+let sum:number =addNumber(10,20);
+console.log('Sum of two numbers:'+sum);
